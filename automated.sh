83fa5b508f17d976e7c35cb1e921288540ebdd0f
osendott <william.osendott@gmail.com>
2015-05-17 20:28:56 -0400

    merge changes from testing repo

--- automated.sh
+++ automated.sh
@@ -1,38 +1,84 @@
-#!/bin/bash
-################################################################################
-#script will fully automate the theming & generation of cursor files
-#written by: William Osendott
-################################################################################
+#############################################
+# noname-cursors v0.9.8 rewrite 17 MAY 2015 #
+# by: William Osendott  & Umut Topuzoglu    #
+#############################################
 
-###########################################################################
+# set -x # uncomment this if you wanna see what's going on line-by-line, remove before distribution
 
 #############
-# variables # default values - variables will be set by script when ran
+# variables #
 #############
-themeStyle=light # light/dark
-newColor=#d64933 # any hex-color
-oldColor=#d64933 # any hex-color
-# fileSource - holds .svg filg in create_png
-count=0 # counter used to keep track of files converted/generated
+choice="" # light/dark response
+themeStyle="" # light/dark variable
+tmpColor="" # hold user color choice @ menu
+newColor="" # theme color to replace Default
+usrColor="" # custom color from dialog
+exitcode="" # custom color dialog button press
+count="" # counter
 CHANGEDIR=$PWD/src # change to source directory in sub-shell
 OUTDIR=$PWD/theme/custom-cursors/cursors # where to generate files
-# CURSOR - will hold the name of .cursor file in generate_xmc
-# BASENAME - holds of cursor have path + extension stripped in generate_xmc
 
-###########################################################################
-
-#############
-# functions # **list of functions goes here **
-#############
-
-startup()
-{
+# extract source files
 tar -xzf src.tar.gz
 tar -xzf theme.tar.gz
+wait
+
+# light/dark menu ($themeStyle)
+dialog --yes-label "light" --no-label "dark" --yesno "Please choose base:" 6 25
+choice=$?
+case $choice in
+  1) (cd $PWD/src;
+        find . -type f -name '*.svg' -print0 | while IFS= read -r -d '' file; do
+sed -i "s/#e8e8e8/#ff0000/g" "$file"
+sed -i "s/#2d2d2d/#e8e8e8/g" "$file"
+sed -i "s/#ff0000/#2d2d2d/g" "$file"
+sed -i "s/#ffffff/#000000/g" "$file"
+done)
+wait
+echo "dark base generated..."
+esac
+
+get_Color()
+{
+  usrColor=$(dialog --inputbox "Enter hex-code:" 8 40 2>&1 >/dev/tty)
 }
 
-theme()
-{
+dialog --radiolist "Select color:" 20 25 25\
+  1 "Default" on \
+  2 "Blue" off \
+  3 "Brown" off \
+  4 "Green" off \
+  5 "Grey" off \
+  6 "Pink" off \
+  7 "Purple" off \
+  8 "Red" off \
+  9 "Yellow" off \
+  10 "Custom" off \
+2> color.tmp
+
+tmpColor=$(<color.tmp)
+case $tmpColor in
+  1) newColor="#d64933" ;;
+  2) newColor="#42a5f5" ;;
+  3) newColor="#8d6e63" ;;
+  4) newColor="#66bb6a" ;;
+  5) newColor="#bdbdbd" ;;
+  6) newColor="#f06292" ;;
+  7) newColor="#7e57c2" ;;
+  8) newColor="#ef5350" ;;
+  9) newColor="#ffca28" ;;
+  10) get_Color
+
+if ! [[ $usrColor =~ ^#[0-9A-Fa-f]{6}$ ]]; then
+    usrColor=""
+    get_Color
+  else
+    newColor=$usrColor
+fi ;;
+esac
+
+
+# recolor
 (cd $PWD/src;
 find . -type f -name '*.svg' -print0 | while IFS= read -r -d '' file; do
 if [[ `grep "$oldColor" "$file"` ]]; then
@@ -41,11 +87,9 @@ sed -i "s/$oldColor/$newColor/g" "$file"
 wait
 fi
 done)
-}
 
-generate_png()
-{
-count=0  
+# create .png files
+count=0
 for fileSource in $PWD/src/*.svg
 do
 if [ -f "$fileSource" ]; then
@@ -56,15 +100,11 @@ inkscape $fileSource --export-png=$file.png --export-dpi=90
 wait
 else
 echo "no file $fileSource found!"
-exit 
+exit
 fi
 done
 
-echo "$count file(s) converted"
-}
-
-create_cursors()
-{
+# create cursor files
 count=0
 for CURSOR in $PWD/src/*.cursor; do
 BASENAME=$CURSOR
@@ -75,109 +115,11 @@ count=$((count+1))
 (cd $CHANGEDIR;xcursorgen $BASENAME.cursor $OUTDIR/$BASENAME)
 wait
 done
-}
 
-install_cursors()
-{
+# install theme
 cp $PWD/theme/custom_cursors/. ~/.icons/custom-cursors/ -R
-}
 
-cleanup()
-{
+# clean everything up
 rm -rf $PWD/src
 rm -rf $PWD/theme
-}
-
-menu()
-{
-echo "Choose main color:"
-echo "default, blue, brown, green, grey, orange, pink, purple, red, yellow"
-echo "or enter a custom hex-code, starting with #"
-read -p ">> " newColor
-printf "\n"
-}
-
-theme_dark()
-{
-(cd $PWD/src;
-        find . -type f -name '*.svg' -print0 | while IFS= read -r -d '' file; do
-sed -i "s/#e8e8e8/#ff0000/g" "$file"
-sed -i "s/#2d2d2d/#e8e8e8/g" "$file"
-sed -i "s/#ff0000/#2d2d2d/g" "$file"
-sed -i "s/#ffffff/#000000/g" "$file"
-done)
-echo "dark base generated..."
-}
-
-###########################################################################
-
-startup
-
-########
-# MENU # will be replaced with a proper menu next edit.
-########
-
-echo "Light or Dark base?"
-read -p "(default: light) >>" themeStyle
-printf "\n"
-
-menu
-
-if [[ $themeStyle == d* ]]; then
-theme_dark
-else
-echo "using light base..."
-fi
-
-case "$newColor" in
-"default" )
-newColor=#d64933 
-;;
-"blue" )
-newColor=#42a5f5 
-;;
-"brown" )
-newColor=#8d6e63 
-;;
-"green" )
-newColor=#66bb6a 
-;;
-"grey" )
-newColor=#bdbdbd 
-;;
-"orange" )
-newColor=#f57c00 
-;;
-"pink" )
-newColor=#f06292 
-;;
-"purple" )
-newColor=#7e57c2 
-;;
-"red" )
-newColor=#ef5350 
-;;
-"yellow" )
-newColor=#ffca28 
-;;
-esac
-
-# make sure color-code is valid
-if ! [[ $newColor =~ ^#[0-9A-Fa-f]{6}$ ]]; then
-echo -e \ "Error, choose a valid color, if using a custom hex-code, don't forget the #. Try again..."
-echo ""
-menu
-fi
-
-theme
-generate_png
-create_cursors
-install_cursors
-cleanup
-
-
-
-
-
-
-################################################################################
+rm -rf $PWD/color.tmp
